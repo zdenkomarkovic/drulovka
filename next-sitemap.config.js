55fdf1a0d540177eed2eb7dac17cb972d4b5cbb5
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2025-04-02 00:47:52 +0200

    .

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://.iznajmljivanjeskelanis.rs", // Zameni svojim domenom
+  generateRobotsTxt: true,
+  sitemapSize: 5000,
+};
